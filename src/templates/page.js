28f906817f3c8f587cbb4c63ab8968bbf6193d27
mor10 <morten@pinkandyellow.com>
2020-11-20 11:35:52 -0800

    Add featured image feature

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Img from "gatsby-image"
 
 import style from "./single.module.css"
 import Layout from "../components/layout"
@@ -11,6 +12,14 @@ export default ({ data }) => {
     <Layout>
       <SEO title={page.title} />
       <article className={style.article}>
+        {page.featuredImage && (
+          <figure className={style.featimg}>
+            <Img
+              fluid={page.featuredImage.node.localFile.childImageSharp.fluid}
+              alt={page.featuredImage.node.altText}
+            />
+          </figure>
+        )}
         <h1>{page.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: page.content }} />
       </article>
@@ -23,6 +32,18 @@ export const query = graphql`
     wpPage(databaseId: { eq: $databaseId }) {
       title
       content
+      featuredImage {
+        node {
+          altText
+          localFile {
+            childImageSharp {
+              fluid(maxWidth: 1360) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
     }
   }
 `
