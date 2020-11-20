a1b6939904a7e01de8ad3ebbffc6a053d2fac4f7
mor10 <morten@pinkandyellow.com>
2020-11-20 10:55:49 -0800

    End-state

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,13 +1,20 @@
 import React from "react"
 import { graphql } from "gatsby"
 
+import style from "./single.module.css"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
 export default ({ data }) => {
   const page = data.wpPage
   return (
-    <article>
-      <h1>{page.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: page.content }} />
-    </article>
+    <Layout>
+      <SEO title={page.title} />
+      <article className={style.article}>
+        <h1>{page.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: page.content }} />
+      </article>
+    </Layout>
   )
 }
 
