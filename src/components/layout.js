9487d49a0cd4e30aeeff4b2dfed979f48d4ccd09
mor10 <morten@pinkandyellow.com>
2020-11-21 15:18:47 -0800

    Add block styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,6 +12,7 @@ import { useStaticQuery, graphql } from "gatsby"
 import Header from "./header"
 import Footer from "./footer"
 import "./layout.css"
+import "@wordpress/block-library/build-style/style.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
