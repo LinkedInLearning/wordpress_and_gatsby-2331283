23f0a110f92763a64c606c3513d36521e70a5892
mor10 <morten@pinkandyellow.com>
2020-11-21 10:45:17 -0800

    Add postNav component

---
+++ src/components/postNav.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { Link } from "gatsby"
+import style from "./PostNav.module.css"
+
+const PostNav = ({ prevPost, nextPost }) => {
+  return (
+    <nav className={style.postnav}>
+      {prevPost && (
+        <Link to={`/posts${prevPost.uri}`}>
+          <div className={style.postnav__head}>Previous post:</div>
+          <div className={style.postnav__title}>{prevPost.title}</div>
+        </Link>
+      )}
+
+      {nextPost && (
+        <Link to={`/posts${nextPost.uri}`}>
+          <div className={style.postnav__head}>Next post:</div>
+          <div className={style.postnav__title}>{nextPost.title}</div>
+        </Link>
+      )}
+    </nav>
+  )
+}
+
+export default PostNav
